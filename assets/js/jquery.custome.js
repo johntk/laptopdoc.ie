8d2de30068ed3383be896f087b5717dbb5e1d576
johntk <johntk86@gmail.com>
2015-01-06 16:02:10 +0000

    Push laptopdoc.ie to git

---
+++ assets/js/jquery.custome.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+	$('.item').mouseenter(function () {
+		$('.item').fadeTo('fast', 0.8);
+	});
+	$('.item').mouseleave(function () {
+		$('.item').fadeTo('slow', 1);
+
+	});
+});
